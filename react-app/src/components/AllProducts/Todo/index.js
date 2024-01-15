65276e2fa780911b5b6c8c421d01947b9c09de88
iam-gopi <pgopirmd@gmail.com>
2024-01-16 00:18:51 +0530

    design updated

--- react-app/src/components/AllProducts/Todo/index.js
+++ react-app/src/components/AllProducts/Todo/index.js
@@ -1,40 +1,45 @@
 import React from "react";
+import ProductDetails from "../common";
 
 export default function Todo() {
   return (
-    <section
-      data-aos="fade-up"
-      data-aos-duration="1000"
-      className="grid max-w-screen-xl mx-auto"
-    >
-      <p>
-        Create your own workflows with custom task lists for managing your
-        tickets.
-      </p>
-      <p>
-        Businesses always have a set of processes and rules that need to be
-        adhered to in their day to day operations. The Task To do App
-        exclusively built for FreshDesk comes in handy to structure this process
-        for a newly joined team member and as well as a seasoned staff.
-      </p>
-      <p>
-        Once the app is installed, the admin of the organization would be able
-        to
-      </p>
-      <p>
-        1. Add a set of tasks for ticket type or dependent fields that agents
-        must complete. <br /> 2. All created tasks that are critical or
-        necessary can be marked as mandatory so that the agents have to complete
-        them before resolving or closing the ticket.
-      </p>
-      <p>Once the app is configured, inside the ticket details page </p>
-      <p>
-        1. Both agents and admin would be able to see the tasks shown for a
-        selected ticket type. <br /> 2. Agents can refer to the tasks to ensure
-        all steps are followed while resolving that particular ticket. <br /> 3.
-        Based on the app settings, agents will be able to resolve or close a
-        ticket if all tasks are not checked and completed.
-      </p>
-    </section>
+    <>
+      {/* <section
+        data-aos="fade-up"
+        data-aos-duration="1000"
+        className="grid max-w-screen-xl mx-auto"
+      >
+        <p>
+          Create your own workflows with custom task lists for managing your
+          tickets.
+        </p>
+        <p>
+          Businesses always have a set of processes and rules that need to be
+          adhered to in their day to day operations. The Task To do App
+          exclusively built for FreshDesk comes in handy to structure this
+          process for a newly joined team member and as well as a seasoned
+          staff.
+        </p>
+        <p>
+          Once the app is installed, the admin of the organization would be able
+          to
+        </p>
+        <p>
+          1. Add a set of tasks for ticket type or dependent fields that agents
+          must complete. <br /> 2. All created tasks that are critical or
+          necessary can be marked as mandatory so that the agents have to
+          complete them before resolving or closing the ticket.
+        </p>
+        <p>Once the app is configured, inside the ticket details page </p>
+        <p>
+          1. Both agents and admin would be able to see the tasks shown for a
+          selected ticket type. <br /> 2. Agents can refer to the tasks to
+          ensure all steps are followed while resolving that particular ticket.{" "}
+          <br /> 3. Based on the app settings, agents will be able to resolve or
+          close a ticket if all tasks are not checked and completed.
+        </p>
+      </section> */}
+      <ProductDetails />
+    </>
   );
 }
